fcbcc7c1997449dc93b729a4e9c3af124b30e3dd
KyryloYehorov <Kyrylo.Yehorov@cit.khpi.edu.ua>
2023-01-23 14:54:19 -0500

    00

---
+++ client/inc/header.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>	//strlen
+#include<stdlib.h>	//strlen
+#include<sys/socket.h>
+#include<arpa/inet.h>	//inet_addr
+#include<unistd.h>	//write
+#include<pthread.h> //for threading , link with lpthread
+
+void *connection_handler(void *);
