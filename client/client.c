ac45b3bff83896c93ce7565d796f108b0fae5fc4
karinayyy <karinakarpenko.k@gmail.com>
2023-02-03 17:31:01 +0200

    local path to theme.css

--- client/client.c
+++ client/client.c
@@ -95,7 +95,7 @@ app_activate(GApplication *app)
 
 	GtkCssProvider *provider = gtk_css_provider_new();
 	gtk_css_provider_load_from_path(provider,
-									"/home/criops/test_uchat/ucode-connect-uchat/client/style/theme.css");
+									"client/style/theme.css");
 
 	GtkStyleContext *context = gtk_widget_get_style_context(win);
 	gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
