cbd711b3fb8e2959065abfe39b14c42b1ed4fb8a
criops <criops@criops>
2023-02-06 17:07:38 +0200

    added ui buttons into main_menu_box, added search box template

--- client/src/home_page.c
+++ client/src/home_page.c
@@ -12,17 +12,32 @@ void show_home_page_screen(void) {
 
     GtkWidget *profile_avatar_btn = gtk_button_new();
 
-   // GtkWidget *home_btn = gtk_button_new();
+    GtkWidget *home_btn = gtk_button_new();
+
+    GtkWidget *chats_btn = gtk_button_new();
+
+    GtkWidget *settings_btn = gtk_button_new();
+
+    GtkWidget *achievements_btn = gtk_button_new();
+
+    GtkWidget *log_out_btn = gtk_button_new();
 
 
+    //box for search_bar
+    GtkWidget *search_bar_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 
-
-    
     //search bar on top of the screen
     GtkWidget *search_bar = gtk_search_bar_new();
 
-    //box for search_bar
-    GtkWidget *search_bar_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    GtkWidget *search_btn = gtk_button_new();
+
+    GtkWidget *entry_for_search = gtk_entry_new();
+
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry_for_search), " Search");
+
+
+
+
 
     //groups box
     GtkWidget *groups_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
@@ -55,50 +70,65 @@ void show_home_page_screen(void) {
 
     //
     gtk_box_append(GTK_BOX(main_menu_box), profile_avatar_btn);
-    //gtk_box_append(GTK_BOX(main_menu_box), home_btn);
+    gtk_box_append(GTK_BOX(main_menu_box), home_btn);
+    gtk_box_append(GTK_BOX(main_menu_box), chats_btn);
+    gtk_box_append(GTK_BOX(main_menu_box), settings_btn);
+    gtk_box_append(GTK_BOX(main_menu_box), achievements_btn);
+    gtk_box_append(GTK_BOX(main_menu_box), log_out_btn);
+
+
 
     //profile_avatar_btn
     gtk_widget_set_margin_start(profile_avatar_btn, 29);
     gtk_widget_set_margin_end(profile_avatar_btn, 29);
     gtk_widget_set_margin_top(profile_avatar_btn, 35);
-    gtk_widget_set_margin_bottom(profile_avatar_btn, 565);
 
     gtk_widget_set_size_request(profile_avatar_btn, 67, 67);
 
     //home_btn
-    // gtk_widget_set_margin_start(home_btn, 29);
-    // gtk_widget_set_margin_end(home_btn, 29);
-    // gtk_widget_set_margin_top(home_btn, 35);
-    // gtk_widget_set_margin_bottom(home_btn, 565);
+    gtk_widget_set_margin_start(home_btn, 47);
+    gtk_widget_set_margin_end(home_btn, 46);
+    gtk_widget_set_margin_top(home_btn, 52);
 
-    // gtk_widget_set_size_request(home_btn, 67, 67);
+    gtk_widget_set_size_request(home_btn, 32, 32);
+
+    //chats_btn
+    gtk_widget_set_margin_start(chats_btn, 46);
+    gtk_widget_set_margin_end(chats_btn, 46);
+    gtk_widget_set_margin_top(chats_btn, 54);
+
+    gtk_widget_set_size_request(chats_btn, 32, 32);
+
+    //settings_btn
+    gtk_widget_set_margin_start(settings_btn, 47);
+    gtk_widget_set_margin_end(settings_btn, 46);
+    gtk_widget_set_margin_top(settings_btn, 54);
+
+    gtk_widget_set_size_request(settings_btn, 32, 32);
+
+    //achievements_btn
+    gtk_widget_set_margin_start(achievements_btn, 47);
+    gtk_widget_set_margin_end(achievements_btn, 46);
+    gtk_widget_set_margin_top(achievements_btn, 54);
+
+    gtk_widget_set_size_request(achievements_btn, 32, 32);
+
+    //log_out_btn
+    gtk_widget_set_margin_start(log_out_btn, 47);
+    gtk_widget_set_margin_end(log_out_btn, 46);
+    gtk_widget_set_margin_top(log_out_btn, 160);
+
+    gtk_widget_set_size_request(log_out_btn, 32, 32);
 
     
     //styling
     widget_styling(main_menu_box, curent_screen, "main_box_style");
     widget_styling(profile_avatar_btn, curent_screen, "profile_avatar_btn");
-    //widget_styling(home_btn, curent_screen, "home_btn");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    widget_styling(home_btn, curent_screen, "home_btn");
+    widget_styling(chats_btn, curent_screen, "chats_btn");
+    widget_styling(settings_btn, curent_screen, "settings_btn");
+    widget_styling(achievements_btn, curent_screen, "achievements_btn");
+    widget_styling(log_out_btn, curent_screen, "log_out_btn");
 
 
 
@@ -115,11 +145,42 @@ void show_home_page_screen(void) {
 
     gtk_widget_set_size_request(search_bar_box, 427, 59);
 
+
+
+    //packing search_bar_btn into search_bar_box
+    gtk_box_append(GTK_BOX(search_bar_box), search_btn);
+    gtk_box_append(GTK_BOX(search_bar_box), entry_for_search);
+
+    //search_btn
+    gtk_widget_set_margin_start(search_btn, 23);
+    gtk_widget_set_margin_top(search_btn, 15);
+    gtk_widget_set_margin_bottom(search_btn, 15);
+
+    gtk_widget_set_size_request(search_btn, 24, 24);
+
+    //entry_for_search
+    gtk_widget_set_margin_start(entry_for_search, 31);
+    gtk_widget_set_margin_top(entry_for_search, 15);
+    gtk_widget_set_margin_bottom(entry_for_search, 15);
+
+    gtk_widget_set_size_request(entry_for_search, 57, 24);
+
+
+
     //styling
     widget_styling(search_bar_box, curent_screen, "main_box_style");
+    widget_styling(search_btn, curent_screen, "search_btn");
+    widget_styling(entry_for_search, curent_screen, "entry_for_search");
 
-    //packing serch_bar into search_bar_box
-    gtk_box_append(GTK_BOX(search_bar_box), search_bar); // - for future content
+
+
+
+
+
+
+
+
+
 
 
     //groups_box init
@@ -191,3 +252,4 @@ void show_home_page_screen(void) {
 
 
 }
+
