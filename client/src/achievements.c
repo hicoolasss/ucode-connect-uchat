4fd1cc13ba575f4ca9e17331022187d32c1c849b
criops <criops@criops>
2023-02-12 23:23:59 +0200

    fixed git issue

--- client/src/achievements.c
+++ client/src/achievements.c
@@ -4,7 +4,6 @@ extern t_screen curent_screen;
 extern t_grid curent_grid;
 
 void show_achievements() {
-<<<<<<< HEAD
 
 
 
@@ -278,17 +277,5 @@ void show_achievements() {
 
 
 
-=======
-    //change lab to your code
-    GtkWidget *scrolled_window = gtk_label_new("ACHIEVEMENTS");
-    //your main box
-    GtkWidget *main_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);;
 
-    gtk_box_append(GTK_BOX(main_box), scrolled_window);
-
-    //attach your main box to mygrid
-    gtk_grid_attach(GTK_GRID(curent_grid.achievements), main_box, 0, 0, 1, 1);
-    //show to on screen
-    gtk_widget_set_visible(GTK_WIDGET(curent_grid.achievements), TRUE);
->>>>>>> b23eb8e6cbf2a4784f01cb3d6fa6bb56419b7b15
 }
