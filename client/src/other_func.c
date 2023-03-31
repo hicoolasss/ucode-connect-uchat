12ad45543f40f4159c38daf8e5aaae501f785f01
KyryloYehorov <Kyrylo.Yehorov@cit.khpi.edu.ua>
2023-03-31 21:04:27 +0300

    add <user_list> to frontend

--- client/src/other_func.c
+++ client/src/other_func.c
@@ -69,3 +69,55 @@ int recv_all(SSL *sockfd, char *buf, int len)
 
     return 1;
 }
+
+t_list *deserialize_name_list(const char *json_str)
+{
+    cJSON *json_list = cJSON_Parse(json_str);
+    if (json_list == NULL)
+    {
+        return NULL;
+    }
+
+    t_list *head = NULL;
+    t_list *tail = NULL;
+
+    cJSON *json_node = NULL;
+    cJSON_ArrayForEach(json_node, json_list)
+    {
+        cJSON *json_name = cJSON_GetObjectItem(json_node, "name");
+        if (cJSON_IsString(json_name))
+        {
+            t_list *new_node = (t_list *)malloc(sizeof(t_list));
+            new_node->data = mx_strdup(json_name->valuestring);
+            new_node->next = NULL;
+
+            if (head == NULL)
+            {
+                head = new_node;
+                tail = head;
+            }
+            else
+            {
+                tail->next = new_node;
+                tail = tail->next;
+            }
+        }
+    }
+    cJSON_Delete(json_list);
+    return head;
+}
+
+t_list *receive_list(SSL *ssl)
+{
+    const int buffer_size = 4096;
+    char buffer[buffer_size];
+
+    int bytes_received = SSL_read(ssl, buffer, buffer_size - 1);
+    if (bytes_received <= 0)
+    {
+        return NULL;
+    }
+
+    buffer[bytes_received] = '\0';
+    return deserialize_name_list(buffer);
+}
