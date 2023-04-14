d4707c30c1f3af66c821d2067ef8f372980792a0
KyryloYehorov <Kyrylo.Yehorov@cit.khpi.edu.ua>
2023-04-14 15:49:43 +0300

    pthread cancel fix

--- client/src/recv_func.c
+++ client/src/recv_func.c
@@ -5,16 +5,21 @@ void print_message(char *login, char *message);
 void *recv_func()
 {
     char command[2048];
+
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
     while (1)
     {
         mx_printstr("recv\n");
         pthread_mutex_lock(&mutex1);
-        
-        while (!in_chat) {
+
+        while (!in_chat)
+        {
             pthread_cond_wait(&new_data_cond, &mutex1);
         }
         int len = SSL_read(current_client.ssl, command, sizeof(command) - 1);
-        
+
         pthread_mutex_unlock(&mutex1);
         if (len == -1)
         {
@@ -35,7 +40,6 @@ void *recv_func()
         cJSON_Delete(json_obj);
         // data_ready = 0;
     }
-    pthread_detach(pthread_self());
     return NULL;
 }
 
