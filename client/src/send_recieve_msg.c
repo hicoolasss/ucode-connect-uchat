5566af95f3cff79acf66c8189f343dd62c69fe82
KyryloYehorov <Kyrylo.Yehorov@cit.khpi.edu.ua>
2023-04-20 19:35:57 +0300

    merge commit

--- client/src/send_recieve_msg.c
+++ client/src/send_recieve_msg.c
@@ -9,72 +9,6 @@ t_list *all_messages_list = NULL;
 
 extern GtkWidget *chat_with_friend_grid;
 
-// gboolean send_receive_msg(gpointer user_data)
-// {
-
-//     t_chat *message_data = (gpointer)user_data;
-
-//     if (message_data)
-//     {
-
-//         t_chat *new_message = (t_chat *)malloc(sizeof(t_chat));
-//         new_message->id = message_data->id;
-//         new_message->sender = strdup(message_data->sender);
-//         new_message->message = strdup(message_data->message);
-//         new_message->timestamp = strdup(message_data->timestamp);
-
-//         mx_push_back(&all_messages_list, new_message);
-
-//         if (strcmp(message_data->sender, current_client.login) == 0)
-//         {
-//             const char *s_msg = message_data->message;
-
-//             GtkWidget *sent_msg = gtk_label_new(s_msg);
-
-//             gtk_widget_set_halign(sent_msg, GTK_ALIGN_END);
-
-//             gtk_widget_set_margin_top(sent_msg, 15);
-
-//             gtk_label_set_wrap(GTK_LABEL(sent_msg), TRUE);
-//             gtk_label_set_wrap_mode(GTK_LABEL(sent_msg), PANGO_WRAP_WORD_CHAR);
-//             gtk_label_set_max_width_chars(GTK_LABEL(sent_msg), 60);
-//             gtk_label_set_selectable(GTK_LABEL(sent_msg), FALSE);
-
-//             gtk_widget_set_hexpand(sent_msg, TRUE);
-
-//             gtk_grid_attach(GTK_GRID(chat_with_friend_grid), sent_msg, 0, message_data->id, 1, 1);
-
-//             widget_styling(sent_msg, current_screen, "message");
-//         }
-//         else
-//         {
-//             const char *r_msg = message_data->message;
-
-//             GtkWidget *received_msg = gtk_label_new(r_msg);
-
-//             gtk_widget_set_halign(received_msg, GTK_ALIGN_START);
-
-//             gtk_widget_set_margin_top(received_msg, 15);
-
-//             gtk_label_set_wrap(GTK_LABEL(received_msg), TRUE);
-//             gtk_label_set_wrap_mode(GTK_LABEL(received_msg), PANGO_WRAP_WORD_CHAR);
-//             gtk_label_set_max_width_chars(GTK_LABEL(received_msg), 60);
-//             gtk_label_set_selectable(GTK_LABEL(received_msg), FALSE);
-
-//             gtk_widget_set_hexpand(received_msg, TRUE);
-
-//             gtk_grid_attach(GTK_GRID(chat_with_friend_grid), received_msg, 0, message_data->id, 1, 1);
-
-//             widget_styling(received_msg, current_screen, "message");
-//         }
-//         return FALSE;
-//     }
-//     else
-//     {
-//         return FALSE;
-//     }
-// }
-
 void add_message_to_chat_history(t_list **friend_list, const char *username, t_chat *new_chat)
 {
     if (!friend_list || !new_chat)
@@ -169,6 +103,7 @@ void add_new_friend(t_list **friend_list, const char *username)
     friend_data->username = mx_strdup(username);
     friend_data->lastmessage = "Nothing here...";
     friend_data->chat_history = NULL;
+    friend_data->in_chat = false;
 
     t_list *new_friend_node = (t_list *)malloc(sizeof(t_list));
     if (!new_friend_node)
