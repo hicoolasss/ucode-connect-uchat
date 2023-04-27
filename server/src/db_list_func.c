d789fce9aa22eabcc3adae78156b5f194b93413c
KyryloYehorov <Kyrylo.Yehorov@cit.khpi.edu.ua>
2023-04-28 00:01:36 +0300

    merge commit

--- server/src/db_list_func.c
+++ server/src/db_list_func.c
@@ -83,7 +83,6 @@ t_list *get_friends(sqlite3 *db, const char *username)
             {
                 friend_person->avatarname = mx_strdup((const char *)avatarname);
             }
-            printf("%s\n", friend_person->avatarname);
             if (friend_person != NULL)
             {
                 mx_push_back(&friend_list, friend_person);
